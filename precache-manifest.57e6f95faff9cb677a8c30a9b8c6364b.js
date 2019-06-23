af899d6eca5efba0474471254d80adade86ab93a
unknown <ingaleJ@github.com>
2019-06-23 17:31:59 +0530

    Initial dist subtree commit

---
+++ precache-manifest.57e6f95faff9cb677a8c30a9b8c6364b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VeuPWA/robots.txt"
+  },
+  {
+    "revision": "cd8716e7e5f9fd56d2a7",
+    "url": "/VeuPWA/js/chunk-vendors.35dc3f29.js"
+  },
+  {
+    "revision": "849ac5d671a32f52659b",
+    "url": "/VeuPWA/js/app.b3621ed4.js"
+  },
+  {
+    "revision": "97a7e2c540e8891c0ac1",
+    "url": "/VeuPWA/js/about.1bb4f9f2.js"
+  },
+  {
+    "revision": "e5373288f2ad751dd7ee63b3d0b5461c",
+    "url": "/VeuPWA/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/VeuPWA/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/VeuPWA/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "cd8716e7e5f9fd56d2a7",
+    "url": "/VeuPWA/css/chunk-vendors.bbe6c84c.css"
+  }
+];
